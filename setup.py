5ddb083f39c98f9368fc8542ada234736f9bd46c
mile888 <49620251+mile888@users.noreply.github.com>
2021-12-27 09:08:13 +0100

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,3 +1,43 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:e774c9719879d2c8b8f91fcb40401c6b2144f2a1c8149a54b967195e8e67b72c
-size 1662
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+DESCRIPTION = 'Gaussian Process regression with analytical approximation of uncertainty propagation.'
+
+# Setting up
+setup(
+    name="gp-approx",
+    version='1.0',
+    author="Mile Mitrovic",
+    author_email="mitrovich888@gmail.com",
+    description=DESCRIPTION,
+    long_description = file: README.md,
+    long_description_content_type = text/markdown,
+    url = 'https://github.com/mile888/gp-approx',
+    packages=find_packages(),
+    install_requires=['numpy', 'scipy', 'pandas'],
+    keywords=['machine learning', 'gaussian process', 'python', 'taylor approximation', 'moment matching'],
+    classifiers=[
+            "Intended Audience :: Science/Research",
+            "Intended Audience :: Developers",
+            "License :: OSI Approved",
+            "Programming Language :: Python",
+            "License :: OSI Approved ",
+            "Topic :: Software Development",
+            "Topic :: Scientific/Engineering",
+            "Development Status :: 5 - Production/Stable",
+            "Operating System :: Microsoft :: Windows",
+            "Operating System :: Unix",
+            "Operating System :: MacOS",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: 3.8",
+            "Programming Language :: Python :: 3.9",
+            "Programming Language :: Python :: Implementation :: PyPy",
+                ]
+)
